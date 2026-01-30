7f3922c4179df079ce5571493ed7a243b8e34446
梧华 <11917426+stardustknightk@user.noreply.gitee.com>
2026-01-30 17:48:54 +0800

    fitst commit
    # Conflicts:
    #	.gitignore

---
+++ AiChat/src/main/java/com/interview/kevin/AiChat.java
@@ -0,0 +1,10 @@
+package com.interview.kevin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChat {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ AiChat/src/main/java/com/interview/kevin/controller.java
@@ -0,0 +1,4 @@
+package com.interview.kevin;
+
+public class controller {
+}
